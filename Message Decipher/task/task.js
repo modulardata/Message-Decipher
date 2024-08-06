f2c417d0f8cafb50bc3fa986ff7e00e2375529f4
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-08-06 09:04:00 +0200

    Stage 1/4

--- Message Decipher/task/task.js
+++ Message Decipher/task/task.js
@@ -1,2 +1,10 @@
 const input = require('sync-input');
-
+const fs = require('fs');
+fs.readFile('secret-message.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+    console.log(data);
+    console.log('Finished reading the file');
+});
